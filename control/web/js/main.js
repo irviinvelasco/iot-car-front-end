a01a23068954d67fc56ae5c9fe3d61535802c44c
irviinvelasco <143757949+irviinvelasco@users.noreply.github.com>
2024-05-27 20:21:23 -0600

    Update main.js

--- control/web/js/main.js
+++ control/web/js/main.js
@@ -11,7 +11,7 @@ function callApi(estatus) {
   // Hacer una petición para un usuario con ID especifico
   axios
     .get(
-      "https://18.116.15.117/iot-car-control/back-end/apis/setRegistro.php?valorEstatus=" +
+      "https://3.142.132.21/iot-car-control/back-end/apis/setRegistro.php?valorEstatus=" +
         estatus
     )
     .then(function (response) {
